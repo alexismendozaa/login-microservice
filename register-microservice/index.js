2399fd66b5d889f43eb0831f853591f16971ee67
Alexis <afmendozaf@uce.edu.ec>
2025-02-10 16:38:43 -0500

    q

--- register-microservice/index.js
+++ register-microservice/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const registerRoutes = require('./routes/registerRoutes');
 require('dotenv').config();  // Load environment variables from .env file
 const sequelize = require('./db');
 const User = require('./models/User');  // Ensure models are created
 
-const app = express();
-app.use(express.json());
+const app = express();  // Mueve esta línea antes de usar 'app'
+
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());  // Ahora puedes usar 'app' después de la inicialización
 
 // Register routes
 app.use('/api', registerRoutes);

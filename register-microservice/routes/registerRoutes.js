b9f7012054b934deeb4fd0e3494f22b89aa0765f
Alexis <afmendozaf@uce.edu.ec>
2025-02-10 16:53:50 -0500

    Arreglo

--- register-microservice/routes/registerRoutes.js
+++ register-microservice/routes/registerRoutes.js
@@ -5,4 +5,16 @@ const { registerUser } = require('../controllers/registerController');
 // Route to register a new user
 router.post('/register', registerUser);
 
+// En el microservicio de registro
+app.post('/users/check-username', async (req, res) => {
+    const { username } = req.body;
+    const existingUser = await User.findOne({ where: { username } });
+  
+    if (existingUser) {
+      return res.status(400).json({ message: 'Usuario ya registrado' });
+    }
+    return res.status(200).json({ message: 'Usuario disponible' });
+  });
+  
+
 module.exports = router;
